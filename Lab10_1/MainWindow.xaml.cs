ce5b7b584010716977ecf4423b3f621a5d7c05b2
Lauri Piirainen <K8344@student.jamk.fi>
2017-11-08 11:09:24 +0200

    Lab10 task 1 added

---
+++ Lab10_1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab10_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTruck_click(object sender, RoutedEventArgs e)
+        {
+             String add = (Convert.ToInt32(truckTextBlock.Text) + 1).ToString();
+            truckTextBlock.Text = add;
+        }
+
+        private void BtnCar_click(object sender, RoutedEventArgs e)
+        {
+            String add = (Convert.ToInt32(carTextBlock.Text) + 1).ToString();
+            carTextBlock.Text = add;
+        }
+    }
+}
