d0e57ad0cb83da6df07908dd1ea501e42f3754cd
Lauri Piirainen <K8344@student.jamk.fi>
2017-11-08 10:32:17 +0200

    WpfDemo added

---
+++ WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string nimi = txtNimi.Text;
+            txbMessu.Text = nimi + " olet hieno ihminen ";
+        }
+
+        private void cmbVarit_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Selvitetään mikä väri varlittu
+            ListBoxItem li = (ListBoxItem)cmbVarit.SelectedItem;
+            string testi = li.Content.ToString();
+            //string vari = cmbVarit.SelectedValue.ToString();
+            txbMessu.Text = testi;
+            Brush mybrush = Brushes.Red;
+            //huom: tietyt kontrollien ominaisuudet vaativat tietyn tyyppisen muuttujan
+            //tässä Foreground-ominaisuus vaaatii Brush-Tyypin!!!
+            txbMessu.Foreground = mybrush;
+        }
+    }
+}
