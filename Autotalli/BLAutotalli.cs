55e9535202af8cc77f8fbd7fd52c18a39ddd69c8
Lauri Piirainen <K8344@student.jamk.fi>
2017-11-15 11:04:22 +0200

    Autotalli Demo

--- Autotalli/BLAutotalli.cs
+++ Autotalli/BLAutotalli.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace Autotalli
 {
-    public class Autotalli
+    public static class AutoTalli
     {
+        public static List<Auto> HaeAutot()
+        {
+            List<Auto> autot = new List<Auto>();
+            //GUIn testaamista varten keksitään muutama auto
+            Auto a = new Auto();
+            a.Merkki = "Volvo";
+            a.Malli = "V70";
+            a.VM = 2007;
+            a.KM = 200000;
+            a.Hinta = 9000F;
+            a.URL = "VolvoV70.png";
+            autot.Add(a);
+            //Toinenkin auto
+            Auto b = new Auto()
+            { Merkki = "Audi", Malli = "A4", VM = 2010, KM = 10000, Hinta = 10000, URL = "AudiA4.png"};
+            autot.Add(b);
+            //Kolmas
+            Auto c = new Auto()
+            { Merkki = "Saab", Malli = "95", VM = 1996, KM = 1000000, Hinta = 100, URL = ""};
+            autot.Add(c);
+            //
+            Auto d = new Auto()
+            { Merkki = "Wolksvagen", Malli = "Golf", VM = 2001, KM = 281000, Hinta = 1000, URL = "Golf.jpg" };
+            autot.Add(d);
+            //palautus
+            return autot;
+        }
     }
 
     public class Auto
--- Autotalli/MainWindow.xaml.cs
+++ Autotalli/MainWindow.xaml.cs
@@ -20,9 +20,74 @@ namespace Autotalli
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Auto> autot; //muuttuja on käytettävissä kaikissa luokan metodeissa ja tapahtumakäsittelijöissä
+        private const string polku = @"D:\K8344\Olio\TTOS0200\Kuvat\";
+
         public MainWindow()
         {
+            //TÄNNE KOODI JOKA SUORITETAAN KUN IKKUNA AVATAAN.. Vahinko caps
             InitializeComponent();
+            NaytaKuva("autotalli.png");
+            //VE1 Käsin koodaus
+            List<string> merkit = new List<string>();
+            merkit.Add("Audi");
+            merkit.Add("Saab");
+            merkit.Add("Volvo");
+            merkit.Add("Wolksvagen");
+            //VE2 Parempi kysytään LINQ:lla datasta eri automerkit
+           // var result = autot.Select(m => m.Merkki.Distinct());
+            cmbAutot.ItemsSource = merkit;
+
+        }
+
+        private void btnHaeAutot_Click(object sender, RoutedEventArgs e)
+        {
+            //pyydetään BL-kerrokselta autot, ja näytetään ne käyttäjälle
+            autot = AutoTalli.HaeAutot();
+            dgAutot.ItemsSource = autot;
+        }
+
+        private void NaytaKuva(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                url = "puuttuu.png";
+            }
+            //lisätään kuvatiedostojen vakiopolku
+            url = polku +url;
+            //Kuvan näyttö
+            BitmapImage pic = new BitmapImage();
+            pic.BeginInit();
+            pic.UriSource = new Uri(url);
+            pic.EndInit();
+            imgAuto.Stretch = Stretch.Fill;
+            imgAuto.Source = pic;
+        }
+
+        private void dgAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Huom, olemme itse populoineet DataGridin Auto-olioilla, joten kukin DataGridin jäsen/alkio on itseasiassa Auto-olio
+            object obj = dgAutot.SelectedItem;
+            if (obj != null)
+            {
+                Auto valittu = (Auto)obj;
+                NaytaKuva(valittu.URL);
+            }
+        }
+
+        private void btnHaeAudit_Click(object sender, RoutedEventArgs e)
+        {
+            //Näkyviin pelkästään Audi -merkkiset autot
+            var result = autot.Where(m => m.Merkki.Contains("Audi"));
+            dgAutot.ItemsSource = result;
+        }
+
+        private void cmbAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Suodatetaan DataGridiin näkyviin vain valittu automerkki
+            string merkki = cmbAutot.SelectedValue.ToString();
+            var result = autot.Where(m => m.Merkki.Contains(merkki));
+            dgAutot.ItemsSource = result;
         }
     }
 }
