f87eb3fbe8a747d1bc9f3c12cdf96ead68d1a7db
Lauri Piirainen <K8344@student.jamk.fi>
2017-11-15 09:31:11 +0200

    Autotallidemo

---
+++ Autotalli/BLAutotalli.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autotalli
+{
+    public class Autotalli
+    {
+    }
+
+    public class Auto
+    {
+        public string Merkki { get; set; }
+        public string Malli { get; set; }
+        public int VM { get; set; }
+        public int KM { get; set; }
+        public float Hinta { get; set; }
+        public string URL { get; set; }
+    }
+}
