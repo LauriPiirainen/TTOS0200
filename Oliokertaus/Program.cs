01d29090b8b6ef427dc4b104f1492b6718dec08b
Lauri Piirainen <K8344@student.jamk.fi>
2017-11-22 10:45:01 +0200

    Started "kertaus" task

---
+++ Oliokertaus/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oliokertaus
+{
+    public class AMK
+    {
+        public List <Tutkinto> tutkinnot = new List<Tutkinto>();
+        public string Tutkinto;
+        public void HaeTutkinnot()
+        {
+            foreach (var tutkinto in tutkinnot)
+            {
+                Console.WriteLine(tutkinto.TutkintoNimi);
+            }
+        }
+    }
+
+    public class Tutkinto
+    {
+        public string TutkintoNimi;
+        public List<Opintojakso> Opintojaksot { get; set; }
+        public Tutkinto(string tutkinto)
+        {
+            TutkintoNimi = tutkinto;
+            Opintojaksot = new List<Opintojakso>();
+        }
+        public void HaeOpintojaksot()
+        {
+            foreach (var opintojakso in Opintojakso)
+            {
+                Console.WriteLine(Opintojakso.TutkintoNimi);
+            }
+        }
+
+    }
+
+    public class Opintojakso
+    {
+        public string OpintojaksoNimi;
+        public List<Opettaja> Opettajat { get; set; }
+        public List<Opiskelija> Opiskelijat { get; set; }
+        public Opintojakso(string Opintojakso)
+        {
+            OpintojaksoNimi = Opintojakso;
+            Opettajat = new List<Opettaja>();
+            Opiskelijat = new List<Opiskelija>();
+        }
+    }
+
+    public class Opettaja
+    {
+        public string Name { get; set; }
+    }
+    public class Opiskelija
+    {
+        public string Name { get; set; }
+    }
+}
